df83f7e87bdf1f69cd85a4b9514112d88cdf29a4
Teranjit <teranjit@gmail.com>
2018-02-08 19:42:30 +1100

    Scope Objects

---
+++ test/scope_spec.js
@@ -0,0 +1,10 @@
+const Scope = require('../src/scope');
+
+describe('Scope', () => {
+  it('can be constructed and used as an object', () => {
+    const scope = new Scope();
+    scope.aProperty = 1;
+
+    expect(scope.aProperty).toBe(1);
+  });
+});
