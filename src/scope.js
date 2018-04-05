8c18f5e2ad9c870b3c893dec258debab842cb142
Teranjit <teranjit@gmail.com>
2018-04-06 08:31:39 +1000

    minor code refactor

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,10 @@
 const _ = require("lodash");
 
-//initWatchVal is used as a unique reference to intially set watcher.last
+/**
+ * Note 1:  If all digests are clean will short circuit the loop instead of continuing to check watcehrs that are clean. Returning false in lodash forEach short cicuits loop.
+ * NOte 2: Prevents digest from prematurely ending (Note 1) in cases where new watches are added as part of the digest cycle.
+ * 
+ */
 function initWatchVal() {}
 
 function Scope() {
@@ -16,11 +20,12 @@ Scope.prototype.$watch = function(watchFn, listenerFn = () => {}) {
   };
 
   this.$$watchers = [...this.$$watchers, watcher];
+  // Edge case. Note 2
   this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$digestOnce = function() {
-  let dirty;
+  let isWatchDirty;  //dirty
 
   _.forEach(this.$$watchers, watcher => {
     const newValue = watcher.watchFn(this);
@@ -36,30 +41,29 @@ Scope.prototype.$digestOnce = function() {
         this
       );
 
-      dirty = true;
-      //Optimization: Short ciruits the digest (Note 1)
+      isWatchDirty = true;
+      // Optimization:  Note 1
     } else if (watcher === this.$$lastDirtyWatch) {
       return false;
-      //Returning false in lodash forEach short circuits loop
     }
   });
 
-  return dirty;
+  return isWatchDirty;
 };
 
 Scope.prototype.$digest = function() {
   
-  let timeToLive = 10;
-  let dirty;
+  let timeToLive = 10; // ttl
+  let isWatchDirty;   // dirty
   this.$$lastDirtyWatch = null;
 
   do {
-    dirty = this.$digestOnce();
+    isWatchDirty = this.$digestOnce();
 
-    if (dirty && !timeToLive--) {
+    if (isWatchDirty && !timeToLive--) {
       throw "10 digest iterations reached";
     }
-  } while (dirty);
+  } while (isWatchDirty);
 };
 
 module.exports = Scope;
